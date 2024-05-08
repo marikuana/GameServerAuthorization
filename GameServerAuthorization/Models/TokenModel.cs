4abc898587a2e56d2cf5cf77444751bee79006a1
marikuana <marians22092000@gmail.com>
2024-05-08 20:19:45 +0300

    Added RefreshToken

---
+++ GameServerAuthorization/Models/TokenModel.cs
@@ -0,0 +1,8 @@
+﻿namespace GameServerAuthorization.Models
+{
+    public class TokenModel
+    {
+        public string Accesstoken { get; set; }
+        public string RefreshToken { get; set; }
+    }
+}
